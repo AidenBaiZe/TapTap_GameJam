867e14a04be73d15ef912b4b7340a1a6433b6cbf
marisa <1226734781@qq.com>
2025-10-27 20:30:56 +0800

    提交动画控制器

---
+++ Assets/Animation/AnimationController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Animation
+{
+    public class AnimationController: MonoBehaviour
+    {
+            public Animator animator;
+
+            private int xSpeedHash;
+
+            private int ySpeedHash;
+
+            private int onGroundHash;
+
+            public Rigidbody2D rb;
+            
+            public PlayerMovement playerMovement;
+
+            public void Awake()
+            {
+                animator = GetComponent<Animator>();
+                rb = GetComponent<Rigidbody2D>();
+                playerMovement = GetComponent<PlayerMovement>();
+
+                xSpeedHash = Animator.StringToHash("XSpeed");
+                onGroundHash = Animator.StringToHash("OnGround");
+                ySpeedHash = Animator.StringToHash("YSpeed");
+            }
+
+            void Update()
+            {
+                animator.SetFloat(xSpeedHash, Mathf.Abs(rb.linearVelocityX));
+                animator.SetBool(onGroundHash, playerMovement.isGrounded);
+                animator.SetFloat(ySpeedHash, rb.linearVelocityY);
+            }
+    }
+}
